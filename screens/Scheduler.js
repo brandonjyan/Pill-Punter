78adb28d18a3f6d5b2af93279f631ee4e7479c29
brandonjyan <yanjbrandon@gmail.com>
2021-02-13 22:04:23 -0800

    home and scheduler screens

---
+++ screens/Scheduler.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {
+    View,
+    Text,
+} from "react-native";
+
+const Scheduler = () => {
+    return (
+        <View>
+            <Text>Scheduler</Text>
+        </View>
+    );
+}
+
+export default Scheduler;
